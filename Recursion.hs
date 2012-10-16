e78a7103788442ab21b218584cdb8513cad1f6cc
unknown <cdunphy@SHCGL-IEIEG01.SJRB.AD>
2012-10-16 11:21:57 -0600

    updated slides, also updated the Recursion example

--- Recursion.hs
+++ Recursion.hs
@@ -16,3 +16,10 @@ fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
 -- Get the first 10 fibonacci numbers
 fibs = take 10 $ map fibonacci [1..]
 
+-- Given an amount of money, and a list of coin denominations,
+-- how many ways can you break down the change to make the specified amount?
+countChange :: Int -> [Int] -> Int
+countChange money coins
+	| money == 0 = 1
+	| money < 0 || null coins = 0 -- the null function here checks for an empty list
+	| otherwise = countChange money (tail coins) + countChange (money - head coins) coins
